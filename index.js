2d83ba516f65c3d0da5e684583ce5c6d7585a346
grebett <gregory.betton@gmail.com>
2015-12-09 21:47:26 +0100

    exercise 06: search

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 'use strict'
 
 // new client
+
 let elasticsearch = require('elasticsearch');
 let client = new elasticsearch.Client({
   host: 'localhost:9200', // elasticsearch's default port
   log: 'trace' // this logger prints directly to stdout very useful infos – for more infos, see: https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/logging.html
 });
 
-// populate
-let populate = require('./tools/populate');
+// populate #uncomment if you need to populate the database — be caution, the populate is asynchronous ; if needed, add an EventEmitter or a Promise
+// let populate = require('./tools/populate');
+// populate(client, 'megacorp/employee', require('./data'));
 
-populate(client, 'megacorp/employee', require('./data'));
+client.search({
+  index: 'megacorp',
+  body: {
+    query: {
+      match: {
+        'first_name': 'Rosalyn'
+      }
+    }
+  }
+}, (error, response) => {
+  console.assert(!error, !error ? null : error.message);
+
+  let hits = response.hits;
+
+  console.log(hits.hits);
+});
